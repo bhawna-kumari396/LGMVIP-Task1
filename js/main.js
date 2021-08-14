31ef9479390b2880d1c8ed718acc1296870a56e4
Bhawna Kumari <bhawnakumari.7870@gmail.com>
2021-08-14 16:38:56 +0530

    Done with project setup and add some section

---
+++ js/main.js
@@ -0,0 +1,67 @@
+//Mobile Menu
+
+let nav = document.querySelector("nav");
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+let logoOne = document.querySelector(".logo");
+
+
+
+
+menuBtn.onclick = function (){
+
+    navBar.classList.add("active");
+    menuBtn.style.opacity ="0";
+    menuBtn.style.pointerEvents = "none";
+    body.style.overflow = "hidden";
+
+}
+
+cancelBtn.onclick = function (){
+
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+    body.style.overflow = "auto";
+
+}
+
+
+//Side nevigation Bar close while we click on nevigation links
+
+let navLinks = document.querySelectorAll(".menu li a");
+for(var i = 0; i < navLinks.length; i++){
+    navLinks[i].addEventListener("click", function(){
+        navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+    body.style.overflow = "auto";
+
+    })
+}
+
+
+
+//scroll fixed
+
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("sticky");
+        logoOne.classList.add("active");
+        menuBtn.classList.add("active")
+    }else{
+        nav.classList.remove("sticky");
+        logoOne.classList.remove("active");
+        menuBtn.classList.remove("active")
+    }
+}
+
+
+
+//counter on scroll
+
+
+
+//silk slider testmonial
