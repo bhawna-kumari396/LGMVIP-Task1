3d1e2b64412c309fe0007c36370812d7340502cb
Bhawna Kumari <bhawnakumari.7870@gmail.com>
2021-08-15 15:14:58 +0530

    Project Done Only Counter section Is not working ASAP I update this code

--- js/main.js
+++ js/main.js
@@ -56,12 +56,46 @@ window.onscroll = function () {
         logoOne.classList.remove("active");
         menuBtn.classList.remove("active")
     }
+
+    //counter on scroll
+
+var number = 0;
+var oTop = $('#counter').offset().top - window.innerHeight;
+
+if(number == 0 && $(window).scrollTop() > oTop){
+    var $this = $(this),
+    countTo = $this.attr('data-count');
+    $({
+        countNum: $this.text()
+    }).animate({
+        countNum:countTo
+    },
+    {
+        duration: 2000,
+        easing: 'swing',
+        step: function(){
+            $this.text(Math.floor(this.countNum));
+        },
+        complete:function(){
+            $this.text(this.countNum);
+        }
+    
+    });
+
 }
 
 
-
-//counter on scroll
-
-
+}
 
 //silk slider testmonial
+
+$('.testimonials').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll:1,
+    autoplay:true,
+    cssEase:'linear',
+    slide:'li',
+    arrows: false,
+  });
